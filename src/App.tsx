a74da728a6cd363ffa807b509397d69e51f14c84
Carlosra10 <carloseduardoramirez10@gmail.com>
2024-08-15 11:53:17 -0400

    4 commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import MenuItem from "./components/MenuItem"
+import OrderContents from "./components/OrderContents"
 import { menuItems } from "./data/db"
 import useOrder from "./hooks/useOrder"
 
 function App() {
-  const {addItem} = useOrder()//no se le pasa ningun argumento pero se extrae addItem de useOrder
+  const {order, addItem} = useOrder()//no se le pasa ningun argumento pero se extrae addItem de useOrder
   //Fragment
   return (
     <>
       <header className="bg-teal-400 py-5">
         <h1 className="text-center text-4xl font-black">Calculadora de Propinas y Consumo</h1>
       </header>
-      <main className=" max-w-7xl mx-auto py-20 grid md:grid-cols-2">
+      <main className=" max-w-7xl mx-auto py-20 grid md:grid-cols-2"> 
         <div className="p-5 ">
           <h2 className="text-4xl font-black ">Menú</h2>
           <div className="space-y-3 mt-10">
@@ -24,9 +25,10 @@ function App() {
           ))}
           </div>
         </div>
-        <div>
-          <h2>Consumo</h2>
-
+        <div className="border border-dashed border-slate-300 p-5 rounded-lg space-y-10">
+          <OrderContents
+          order={order}
+          />
         </div>
       </main>
     </>
